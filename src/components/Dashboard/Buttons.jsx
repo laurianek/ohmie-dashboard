1ff0278eb696e3d16ebefc945ef02dd1ee08a50c
Lauriane <lauriane.kayungu@gmail.com>
2022-10-20 13:42:39 +0200

    wip styling

--- src/components/Dashboard/Buttons.jsx
+++ src/components/Dashboard/Buttons.jsx
@@ -1,4 +1,4 @@
-import { XMarkIcon } from '@heroicons/react/24/outline/index.js';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline/index.js';
 
 export function NavCloseButton({ onClick }) {
   return (
@@ -9,6 +9,19 @@ export function NavCloseButton({ onClick }) {
     >
       <span className="sr-only">Close sidebar</span>
       <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+    </button>
+  );
+}
+
+export function MenuButton({ onClick }) {
+  return (
+    <button
+      type="button"
+      className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-lisbon-50 hover:text-lisbon-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-paris-400"
+      onClick={onClick}
+    >
+      <span className="sr-only">Open sidebar</span>
+      <Bars3Icon className="h-6 w-6" aria-hidden="true" />
     </button>
   );
 }
