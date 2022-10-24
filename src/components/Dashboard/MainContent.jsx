3bde2ef61e99c1ce3e8539033cd2dd05439db81f
Lauriane <lauriane.kayungu@gmail.com>
2022-10-24 05:46:06 +0200

    Live demo site

--- src/components/Dashboard/MainContent.jsx
+++ src/components/Dashboard/MainContent.jsx
@@ -4,6 +4,8 @@ import { useStore } from '../../store.jsx';
 
 export default function Main({ children, openSidebar }) {
   const { isLoading, fetchData, data } = useStore();
+  const title = 'Dashboard Demo';
+  const banner = 'this is a demo version of Ohmakase.';
 
   const fetchNewData = () => {
     fetchData();
@@ -12,7 +14,10 @@ export default function Main({ children, openSidebar }) {
 
   return (
     <div className="flex flex-1 flex-col md:pl-64">
-      <div className="sticky top-0 z-10 bg-lisbon-500 text-lisbon-50 px-1 sm:pl-3 sm:py-1.5 flex items-center relative justify-between md:hidden">
+      <div className="sticky top-0 z-10 md:hidden px-0.5 flex text-white justify-center items-center bg-lime-500 text-sm">
+        {banner}
+      </div>
+      <div className="sticky top-[20px] z-10 bg-lisbon-500 text-lisbon-50 px-1 sm:pl-3 sm:py-1.5 flex items-center relative justify-between md:hidden">
         <MenuButton onClick={openSidebar} />
         {isLoading ? (
           <div />
@@ -27,13 +32,16 @@ export default function Main({ children, openSidebar }) {
           </div>
         )}
         <h1 className="text-2xl font-semibold absolute left-0 right-0 text-center top-2 sm:top-3 pointer-events-none">
-          Dashboard
+          {title}
         </h1>
       </div>
       <main className="flex-1">
+        <div className="mx-auto max-w-7xl px-0.5 hidden md:flex text-white justify-center items-center bg-lime-500 mb-2">
+          {banner}
+        </div>
         <div className="py-6">
-          <div className="mx-auto max-w-7xl px-4 md:px-6 lg:px-8 hidden md:flex flex-row text-lisbon-100 justify-between items-center pb-2.5 ">
-            <h1 className="text-2xl font-semibold">Dashboard</h1>
+          <div className="mx-auto max-w-7xl px-4 md:px-6 lg:px-8 hidden md:flex flex-row text-lisbon-100 justify-between items-center pb-2.5">
+            <h1 className="text-2xl font-semibold">{title}</h1>
             {isLoading ? (
               <div />
             ) : (
